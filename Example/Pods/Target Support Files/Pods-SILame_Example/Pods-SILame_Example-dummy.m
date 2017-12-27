7404ed4ba61a082a47787e9b7cfc8de961997f36
ungacy <ungacy@126.com>
2017-12-27 10:18:56 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-SILame_Example/Pods-SILame_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SILame_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SILame_Example
+@end
---
+++ Example/Pods/Target Support Files/SILame/SILame-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SILame : NSObject
+@end
+@implementation PodsDummy_SILame
+@end
